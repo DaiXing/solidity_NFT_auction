01c7c83dfb70d3243c0ea88e955c0aa8eb3331f5
cat <673176230@qq.com>
2026-05-16 23:31:10 +0800

    拍卖逻辑

--- src/NftAuction.sol
+++ src/NftAuction.sol
@@ -21,7 +21,8 @@ contract MyNFT is ERC721URIStorage {
 // 拍卖的状态
 enum AuctionState {
     Success,
-    Fail
+    Fail,
+    Cancel
 }
 // 拍卖的信息
 struct AuctionData {
@@ -31,8 +32,8 @@ struct AuctionData {
     uint256 beginTime; // 开始时间
     uint256 periodTime; // 持续时间
     AuctionState state; // 状态。
-    address bider; // 出价者。
-    uint256 biderPrice; // 出价的金额。
+    address bidder; // 出价者。
+    uint256 bidPrice; // 出价的金额。
 }
 
 contract Auction {
@@ -44,13 +45,28 @@ contract Auction {
     // key=auctionID
     mapping(uint256 => AuctionData) auctionMap;
 
-    event AuctionCreated(
+    // 创建。
+    event AuctionCreate(
         address tokenOwner,
         uint256 tokenId, // 用Go取日志。这里还需要索引吗？
         uint256 auctionId,
         uint256 minPrice,
         uint256 beginTime,
         uint256 periodTime
+    );
+    // 退款。
+    event AuctionRefund(
+        uint256 tokenId, // 用Go取日志。这里还需要索引吗？
+        uint256 auctionId,
+        address to, // 给谁。
+        uint256 amount // 退款金额
+    );
+    // 竞拍。
+    event AuctionBid(
+        uint256 tokenId, // 用Go取日志。这里还需要索引吗？
+        uint256 auctionId,
+        address bidder, // 竞拍人。
+        uint256 bidPrice // 竞拍金额
     );
 
     constructor() {
@@ -89,7 +105,7 @@ contract Auction {
         auctionData.periodTime = periodTime;
 
         // 事件。
-        emit AuctionCreated(
+        emit AuctionCreate(
             msg.sender,
             tokenId,
             auctionId,
@@ -97,5 +113,46 @@ contract Auction {
             beginTime,
             periodTime
         );
+    }
+
+    // 竞拍。
+    function bidAuction(uint256 auctionId) public payable {
+        uint256 amount = msg.value;
+        require(amount > 0, "amount is invalid");
+        AuctionData storage auctionData = auctionMap[auctionId];
+        require(auctionData.beginTime > 0, "auction not found");
+        // 判断时间
+        require(block.timestamp > auctionData.beginTime, "not begin");
+        require(
+            auctionData.beginTime + auctionData.periodTime > block.timestamp,
+            "auction expire"
+        );
+        // 不能重复。
+        require(auctionData.bidder == msg.sender, "bid repeated");
+        // 金额必须够
+        require(amount > auctionData.bidPrice, "amount is small");
+
+        uint256 tokenId = auctionData.tokenId;
+
+        // 退款。
+        address oldbidder = auctionData.bidder;
+        uint256 oldbidPrice = auctionData.bidPrice;
+
+        // 出价更高。保存价格。
+        auctionData.bidder = msg.sender;
+        auctionData.bidPrice = amount;
+
+        // 退款。
+        if (oldbidder != address(0) && oldbidPrice > 0) {
+            // 退款。
+            (bool ok, ) = payable(oldbidder).call{value: oldbidPrice}("");
+            require(ok, "refund error");
+
+            // 事件。
+            emit AuctionRefund(tokenId, auctionId, oldbidder, oldbidPrice);
+        }
+
+        // 事件。
+        emit AuctionBid(tokenId, auctionId, msg.sender, amount);
     }
 }
